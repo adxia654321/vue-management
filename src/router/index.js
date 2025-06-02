74260b4a59379fdab59740d262a48c23d1323877
adxia654321 <adxia654321@gmail.com>
2025-06-02 12:09:28 +0800

    首次提交

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import IndexView from '@/views/index/index.vue'
+import ClazzView from '@/views/clazz/index.vue'
+import DeptView from '@/views/dept/index.vue'
+import EmpView from '@/views/emp/index.vue'
+import LogView from '@/views/log/index.vue'
+import StuView from '@/views/stu/index.vue'
+import EmpReportView from '@/views/report/emp/index.vue'
+import StuReportView from '@/views/report/stu/index.vue'
+import LayoutView from '@/views/layout/index.vue'
+import LoginView from '@/views/login/index.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: HomeView
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ]
+})
+
+export default router
